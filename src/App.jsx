ad757b9bfeba12db1508227892ee59aa054cc02c
omnjja <omniahesham910@gmail.com>
2026-02-17 03:03:31 +0200

    initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-cyan-900">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
