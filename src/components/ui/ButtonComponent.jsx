b9ed9643b6b745550cecb413aabce349ec63d6b0
Mariam-111 <mm8133278@gmail.com>
2026-05-04 17:41:40 +0300

    fix(button component): fix Button component issues

--- src/components/ui/ButtonComponent.jsx
+++ src/components/ui/ButtonComponent.jsx
@@ -98,41 +98,6 @@ const ButtonComponent = ({
       {children ?? text}
     </button>
   );
-  if (gradientBorder) {
-    return (
-      <div
-        className={`
-        inline-block
-        p-[1.5px]
-        bg-linear-to-r
-      from-[#1B41AA]
-      to-[#0FB07C]
-      rounded-${rounded}
-  `}
-      >
-        <button
-          type={type}
-          onClick={onClick}
-          className={`
-      w-full h-full
-      bg-white
-      hover:bg-gray-100
-      text-black
-      px-3 py-1
-      rounded-${rounded}
-      transition-all duration-200
-      flex items-center justify-center gap-2
-      ${fullWidth ? "w-full" : ""}
-    `}
-          disabled={disabled}
-        >
-          {children}
-          <span>{text}</span>
-        </button>
-      </div>
-    );
-  }
-  return buttonContent;
 };
 
 export default ButtonComponent;
