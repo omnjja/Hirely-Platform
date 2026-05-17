9eaa13951665135f5f4d097dfa498a101417e1b7
omnjja <omniahesham910@gmail.com>
2026-05-18 00:05:58 +0300

    fix(auth): update Google authentication

--- src/features/auth/pages/AuthCallback.jsx
+++ src/features/auth/pages/AuthCallback.jsx
@@ -1,22 +1,25 @@
+import useAppNavigate from "@/hooks/useAppNavigate";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AuthCallback = () => {
   const navigate = useNavigate();
+  // const {toRoleSelection} = useAppNavigate()
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
+    // const isNewUser = params.get("isNewUser") === "true";
 
     if (token) {
       localStorage.setItem("authToken", token);
-      navigate("/", { replace: true });
+      // isNewUser ? toRoleSelection() : navigate("/", { replace: true });
     } else {
       navigate("/login", { replace: true });
     }
   }, []);
 
-  return <p>Redirecting...</p>; 
+  return <p>Redirecting...</p>;
 };
 
 export default AuthCallback;
--- src/features/auth/services/authService.js
+++ src/features/auth/services/authService.js
@@ -1,4 +1,4 @@
-import api from "../../../lib/api";
+import api from "@/lib/api";
 
 export const signup = async (formData) => {
   const res = await api.post("auth/signup", formData);
@@ -6,8 +6,9 @@ export const signup = async (formData) => {
 };
 
 export const googleAuth = () => {
-  window.location.href = "http://localhost:3000/auth/google/login";
+  window.location.href = "http://76.13.58.192.nip.io:4000/auth/google/login";
 };
+
 
 export const login = async (data) => {
   const res = await api.post("auth/login", data);
