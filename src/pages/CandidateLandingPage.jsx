7cb78869edbcca0c629c1f8f1be8384d69648ec0
omnjja <omniahesham910@gmail.com>
2026-05-20 02:00:33 +0300

    refactor(candidate-landing-page): simplify layout structure

--- src/pages/CandidateLandingPage.jsx
+++ src/pages/CandidateLandingPage.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
-import Card from "@/components/ui/Card";
 import CandidateLandingHero from "@/features/candidate/landing-page/components/CandidateLandingHero";
 import RecomendedForYou from "@/features/candidate/landing-page/components/RecomendedForYou";
 import LandingRecentJobs from "@/features/candidate/landing-page/components/LandingRecentJobs";
@@ -8,29 +6,15 @@ import TopMatchesJobs from "@/features/candidate/landing-page/components/TopMatc
 
 const CandidateLandingPage = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} className="w-full min-h-screen p-3 sm:p-5">
-        <Grid size={{ xs: 12, lg: 9 }}>
-          <div className="flex flex-col gap-6">
-            <CandidateLandingHero />
-
-            <TopMatchesJobs />
-
-            <RecomendedForYou />
-            <LandingRecentJobs />
-          </div>
-        </Grid>
-
-        {/* AI card */}
-        <Grid size={{ xs: 12, lg: 3 }}>
-          <Card className="p-4 sm:p-5 lg:sticky lg:top-20">
-            <p className="text-sm font-medium text-gray-500 mb-4 flex items-center gap-2">
-              ? AI Card
-            </p>
-          </Card>
-        </Grid>
-      </Grid>
-    </Box>
+    <div
+      className="flex flex-1 flex-col gap-6 w-full min-h-screen p-3 sm:p-5"
+      sx={{ flexGrow: 1 }}
+    >
+      <CandidateLandingHero />
+      <TopMatchesJobs />
+      <RecomendedForYou />
+      <LandingRecentJobs />
+    </div>
   );
 };
 
